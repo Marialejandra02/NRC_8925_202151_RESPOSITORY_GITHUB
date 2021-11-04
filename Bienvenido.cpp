17dda1a7014b9ada98f80d0c5c660eb503307d2f
Marialejandra02 <93732728+Marialejandra02@users.noreply.github.com>
2021-11-04 17:54:22 -0500

    Update Bienvenido.cpp
    Modificacion del codigo commit

--- Bienvenido.cpp
+++ Bienvenido.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main ()
 {
   cout <<"Bienvenido a la Materia de POO"<<endl;
-  cout <<"Hola queridos compañeros"<<endl;
+  cout <<"---Hola queridos compañeros---"<<endl;
   return 0;
 }
